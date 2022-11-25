feeba245f1e6b972e6df6c07e940e5ece890b4e2
Dor_av <doravra.mks@gmail.com>
2022-11-25 13:46:03 +0200

    with_max

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import './App.css';
 import Counter from './Counter';
+import {useState} from "react" ; 
+
 
 function App() {
+  const [delta, setDelta] = useState(1)
+  const [Max, setMax] = useState(1)
+  
+  
+  function handleDelta(e){
+    setDelta(Number(e.target.value))
+  }
+  function handleMax(e){
+    setMax(Number(e.target.value))
+  }
+
+  
   return (
     <div className="App">
-        usestate2
-        <p>add line</p>
-        <Counter/>
+        <h1>usestate2</h1>
+        <p>Delta</p>
+        <input type = "number" value = {delta} onChange ={handleDelta} />
+        <p>Max Value</p>
+        <input type = "number" value = {Max} onChange ={handleMax} />
+        <Counter delta = {delta} Max = {Max} />
+        <Counter delta = {delta} Max = {Max} />
     </div>
   );
 }
--- src/Counter.js
+++ src/Counter.js
@@ -1,33 +1,30 @@
 import {useState} from "react" ; 
 import './Counter.css';
 
-function Counter() {
+function Counter(props) {
+    const {delta} = props
     const [count, setCount] = useState(1)
-    const [delta, setDelta] = useState(1)
-
+    const {Max} = props
 
     function incr() {
         setCount(
             function(oldCount){
-                return oldCount + delta
+              if ( oldCount >= Max ) {
+                return 0 }
+              return oldCount + delta
             }
         )
-        console.log(count) // for chaking !
+        //console.log(count) // for chaking !
     }
 
     function Reset() {
         setCount(0) 
           }
 
-    function handleDelta(e){
-      setDelta(Number(e.target.value))
-
-    }
 
     return (
       <div>
         <h1>Counter</h1>
-        <input type = "number" value = {delta} onChange ={handleDelta} />
         <p>Counter us at {count} </p>
         <button onClick={incr}>Click to {delta} add Counter</button>
         <button onClick={Reset} >Click Reset</button>
